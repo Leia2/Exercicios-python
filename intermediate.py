a784a2694ae26f2a259e2c4015d6f8ff9e055b9f
Leia Cardoso <leiacardoso21@gmail.com>
2021-03-01 21:55:30 -0300

    Resolução da questão 80

--- intermediate.py
+++ intermediate.py
@@ -359,3 +359,27 @@ while True:
         print("Password is not fine")
 '''
 #Exercício 80
+#Create a script that lets user submit a password until they have satisfied
+#three conditions:
+#1. Password contains at least one number
+#2. Contains one uppercase letter
+#3. It is at least 5 chars long
+#Give the exact reason why the user has not created a correct password
+
+while True:
+    notes = []
+    psw = input("Enter new password: ")
+    if not any(i.isdigit() for i in psw):
+        notes.append("You need at least one number")
+    if not any(i.isupper() for i in psw):
+        notes.append("You need at least one uppercase letter")
+    if len(psw) < 5:
+        notes.append("You need at least 5 chars long")
+
+    if len(notes) == 0:
+        print("Password is fine")
+    else:
+        print("Please check the following: ")
+        for note in notes:
+            print(note)
+        
